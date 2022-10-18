cf780b9cd7975f7409903e57f493b558c1c1e817
GabrielePagnottae <gabriele.pagnotta1998@gmail.com>
2022-10-18 14:44:48 +0200

    fine esercizio

---
+++ assets/javaprova.js
@@ -0,0 +1,45 @@
+// document.writeln('hello world');
+// alert('hello world');
+// document.getElementById('ciao').innerHTML = '<p>ciao<p>';
+// console.log('ciao');
+
+// let num1 = 10;
+// let num2 = 10;
+// let totale = '';
+
+// totale = num1 + num2;
+
+// console.log(totale);
+
+
+
+
+
+// let età = prompt('Quanti anni hai?');
+// console.log(età);
+// let annoattuale = 2022;
+
+// let annodinascita = età - annoattuale;
+// let frase = `il tuo anno di nascita è $(annodinascita)`;
+
+// document.getElementById(ciao);innerHTML = 'il tuo anno di nascita è' ${annodinascita};
+
+
+// console.log(annodinascita);
+alert('ciao tutor!')
+let name = prompt('come ti chiami?');
+let surname = prompt('il tuo cognome?');
+let color = prompt('quale colore preferisci?');
+console.log(name,surname,color);
+let password = name + surname + color + '24';
+console.log(password);
+let final = `la tua password è ${password}`;
+document.getElementById('container').innerHTML =`"La tua password è ${password}"`;
+
+
+
+
+
+
+
+ 
